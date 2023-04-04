439c7e54e0bcc3a4d628396bdef4a68308c5d657
lolomarka <mark.sheretov@gmail.com>
2023-04-04 18:25:52 +0500

    Constructors

--- Matrix.hpp
+++ Matrix.hpp
@@ -1,16 +1,51 @@
+#include <iostream>
+using namespace std;
+
+template <typename T>
 class Matrix
 {
 private:
-    /* data */
+    T** data;
+    size_t rowsCount;
+    size_t colsCount;
 public:
-    Matrix(/* args */);
+    Matrix();
+    Matrix(size_t rows, size_t cols);
     ~Matrix();
 };
 
-Matrix::Matrix(/* args */)
+template <typename T>
+inline Matrix<T>::Matrix()
 {
+    rowsCount = colsCount = 0;
+    data = nullptr;
 }
 
-Matrix::~Matrix()
+template <typename T>
+inline Matrix<T>::Matrix(size_t rows, size_t cols)
 {
+    rowsCount = rows;
+    colsCount = cols;
+    data = new T**[rowsCount][colsCount];
+    for (size_t i = 0; i < rowsCount; i++)
+    {
+        for (size_t j = 0; j < colsCount; j++)
+        {
+            data[i][j] = default(T);
+        }
+    }
+}
+
+template <typename T>
+inline Matrix<T>::~Matrix()
+{
+    for (size_t i = 0; i < rowsCount; i++)
+    {
+        for (size_t j = 0; j < colsCount; j++)
+        {
+            delete data[i][j];
+        }
+        delete[] data[i];
+    }
+    delete[] data;
 }
