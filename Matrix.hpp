422b934d84fd74c507daff20bbfb4c0082b585e8
lolomarka <mark.sheretov@gmail.com>
2023-04-08 11:15:45 +0500

    Some Changes

--- Matrix.hpp
+++ Matrix.hpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string.h>
+#include <cstring>
 using namespace std;
 
 template <typename T>
@@ -23,12 +24,14 @@ public:
         }
         T &operator*() { return *m_ptr; }
         bool operator!=(const iterator &other) const { return m_ptr != other.m_ptr; }
+        bool operator==(const iterator &other) const { return m_ptr == other.m_ptr; }
 
     private:
         T *m_ptr;
     };
 
     T &operator()(size_t row, size_t col);
+    T &operator=(const Matrix &other);
 
     void Print();
 
@@ -46,6 +49,7 @@ public:
 
     Matrix();
     Matrix(size_t rows, size_t cols);
+    Matrix(const Matrix &other);
     ~Matrix();
 
     iterator begin() { return iterator(data); }
@@ -70,11 +74,27 @@ inline void Matrix<T>::Print(T *d, size_t r, size_t c)
 template <typename T>
 inline T &Matrix<T>::operator()(size_t row, size_t col)
 {
+    using std::invalid_argument;
     if (row < 0 || row >= rowsCount)
-        std::__throw_invalid_argument("Rows");
+        throw invalid_argument("Rows");
     if (col < 0 || col >= colsCount)
-        std::__throw_invalid_argument("Cols");
+        throw invalid_argument("Cols");
     return data[row * colsCount + col];
+}
+
+template <typename T>
+inline T &Matrix<T>::operator=(const Matrix &other)
+{
+    using std::memcpy;
+    if (this != &other)
+    {
+        delete[] data;
+        rowsCount = other.rowsCount;
+        colsCount = other.colsCount;
+        data = new T[rowsCount * colsCount];
+        memcpy(data, other.data, rowsCount * colsCount * sizeof(T));
+    }
+    return *this;
 }
 
 template <typename T>
@@ -86,6 +106,7 @@ inline void Matrix<T>::Print()
 template <typename T>
 inline void Matrix<T>::Swap(Matrix<T> &other)
 {
+    using std::swap;
     swap(colsCount, other.colsCount);
     swap(rowsCount, other.rowsCount);
     swap(data, other.data);
@@ -94,29 +115,39 @@ inline void Matrix<T>::Swap(Matrix<T> &other)
 template <typename T>
 inline size_t Matrix<T>::InsertRow(size_t index, T *elements, size_t elementsLength)
 {
+    using std::invalid_argument;
+    using std::length_error;
     if (elementsLength != colsCount)
-        std::__throw_length_error("Length of row not similar with matrix width.");
+        throw length_error("Length of row not similar with matrix width.");
     if (index < 0 || index > rowsCount)
-        std::__throw_invalid_argument("Index out of range.");
+        throw invalid_argument("Index out of range.");
 
     size_t insertStartIndex = index * colsCount;
     size_t insertEndIndex = insertStartIndex + colsCount;
     size_t newLen = (rowsCount + 1) * colsCount;
-    T *tmpData = new T[newLen];
 
-    for (size_t i = 0; i < insertStartIndex; i++)
-    {
-        tmpData[i] = data[i];
-    }
+    T *tmpData;
 
-    for (size_t i = insertStartIndex; i < insertEndIndex; i++)
+    if (newLen == 0)
+        tmpData = nullptr;
+    else
     {
-        tmpData[i] = elements[i - insertStartIndex];
-    }
+        tmpData = new T[newLen];
 
-    for (size_t i = insertEndIndex; i < newLen; i++)
-    {
-        tmpData[i] = data[insertStartIndex - insertEndIndex + i];
+        for (size_t i = 0; i < insertStartIndex; i++)
+        {
+            tmpData[i] = data[i];
+        }
+
+        for (size_t i = insertStartIndex; i < insertEndIndex; i++)
+        {
+            tmpData[i] = elements[i - insertStartIndex];
+        }
+
+        for (size_t i = insertEndIndex; i < newLen; i++)
+        {
+            tmpData[i] = data[insertStartIndex - insertEndIndex + i];
+        }
     }
 
     delete[] data;
@@ -129,22 +160,31 @@ inline size_t Matrix<T>::InsertRow(size_t index, T *elements, size_t elementsLen
 template <typename T>
 inline size_t Matrix<T>::RemoveRow(size_t index)
 {
+    using std::invalid_argument;
     if (index < 0 || index > rowsCount)
-        std::__throw_invalid_argument("Index out of range.");
+        throw invalid_argument("Index out of range.");
     size_t oldLen = rowsCount * colsCount;
     size_t newLen = (rowsCount - 1) * colsCount;
-    T *tmpData = new T[newLen];
-    size_t removeStartIndex = index * colsCount;
-    size_t removeEndIndex = removeStartIndex + colsCount;
-
-    for (size_t i = 0; i < removeStartIndex; i++)
+    T *tmpData;
+    if (newLen == 0)
     {
-        tmpData[i] = data[i];
+        tmpData = nullptr;
     }
-
-    for (size_t i = removeEndIndex; i < oldLen; i++)
+    else
     {
-        tmpData[removeStartIndex - removeEndIndex + i] = data[i];
+        tmpData = new T[newLen];
+        size_t removeStartIndex = index * colsCount;
+        size_t removeEndIndex = removeStartIndex + colsCount;
+
+        for (size_t i = 0; i < removeStartIndex; i++)
+        {
+            tmpData[i] = data[i];
+        }
+
+        for (size_t i = removeEndIndex; i < oldLen; i++)
+        {
+            tmpData[removeStartIndex - removeEndIndex + i] = data[i];
+        }
     }
 
     delete[] data;
@@ -157,26 +197,36 @@ inline size_t Matrix<T>::RemoveRow(size_t index)
 template <typename T>
 inline size_t Matrix<T>::InsertColumn(size_t index, T *elements, size_t elementsLength)
 {
+    using std::invalid_argument;
+    using std::length_error;
+
     if (elementsLength != rowsCount)
-        std::__throw_length_error("Length of column not similar with matrix width.");
+        throw length_error("Length of column not similar with matrix width.");
     if (index < 0 || index > rowsCount)
-        std::__throw_invalid_argument("Index out of range.");
+        throw invalid_argument("Index out of range.");
     size_t newColsCount = colsCount + 1;
     size_t newLen = rowsCount * (newColsCount);
 
-    T *tmpData = new T[newLen];
-
-    for (size_t i = 0; i < rowsCount; i++)
+    T *tmpData;
+    if (newLen == 0)
     {
-        for (size_t j = 0; j < index; j++)
+        tmpData = nullptr;
+    }
+    else
+    {
+        tmpData = new T[newLen];
+        for (size_t i = 0; i < rowsCount; i++)
         {
-            tmpData[i * newColsCount + j] = data[i * colsCount + j];
-        }
-        auto value = elements[i];
-        tmpData[i * newColsCount + index] = value;
-        for (size_t j = index; j < colsCount; j++)
-        {
-            tmpData[i * newColsCount + j + 1] = data[i * colsCount + j];
+            for (size_t j = 0; j < index; j++)
+            {
+                tmpData[i * newColsCount + j] = data[i * colsCount + j];
+            }
+            auto value = elements[i];
+            tmpData[i * newColsCount + index] = value;
+            for (size_t j = index; j < colsCount; j++)
+            {
+                tmpData[i * newColsCount + j + 1] = data[i * colsCount + j];
+            }
         }
     }
 
@@ -190,22 +240,28 @@ inline size_t Matrix<T>::InsertColumn(size_t index, T *elements, size_t elements
 template <typename T>
 inline size_t Matrix<T>::RemoveColumn(size_t index)
 {
+    using std::invalid_argument;
     if (index < 0 || index > rowsCount)
-        std::__throw_invalid_argument("Index out of range.");
+        throw invalid_argument("Index out of range.");
     size_t newColsCount = colsCount - 1;
     size_t oldLen = rowsCount * colsCount;
     size_t newLen = rowsCount * newColsCount;
-    T *tmpData = new T[newLen];
-
-    for (size_t i = 0; i < rowsCount; i++)
+    T *tmpData;
+    if (newLen == 0)
+        tmpData = nullptr;
+    else
     {
-        for (size_t j = 0; j < index; j++)
+        tmpData = new T[newLen];
+        for (size_t i = 0; i < rowsCount; i++)
         {
-            tmpData[i * newColsCount + j] = data[i * colsCount + j];
-        }
-        for (size_t j = index + 1; j < colsCount; j++)
-        {
-            tmpData[i * newColsCount + j - 1] = data[i * colsCount + j];
+            for (size_t j = 0; j < index; j++)
+            {
+                tmpData[i * newColsCount + j] = data[i * colsCount + j];
+            }
+            for (size_t j = index + 1; j < colsCount; j++)
+            {
+                tmpData[i * newColsCount + j - 1] = data[i * colsCount + j];
+            }
         }
     }
 
@@ -233,18 +289,26 @@ inline void Matrix<T>::Resize(size_t rows, size_t cols)
 {
     if (rows == rowsCount && cols == colsCount)
         return;
-    T *newData = new T[rows * cols];
-    for (size_t i = 0; i < rows; i++)
+    size_t len = rows * cols;
+    T *newData;
+    if (len == 0)
+        newData = nullptr;
+    else
     {
-        for (size_t j = 0; j < cols; j++)
+        newData = new T[len];
+
+        for (size_t i = 0; i < rows; i++)
         {
-            if (i < rowsCount && j < colsCount)
+            for (size_t j = 0; j < cols; j++)
             {
-                newData[i * cols + j] = operator()(i, j);
-            }
-            else
-            {
-                newData[i * cols + j] = T();
+                if (i < rowsCount && j < colsCount)
+                {
+                    newData[i * cols + j] = operator()(i, j);
+                }
+                else
+                {
+                    newData[i * cols + j] = T();
+                }
             }
         }
     }
@@ -258,18 +322,39 @@ template <typename T>
 inline Matrix<T>::Matrix()
 {
     rowsCount = colsCount = 0;
-    data = new T[0];
+    data = nullptr;
 }
 
 template <typename T>
 inline Matrix<T>::Matrix(size_t rows, size_t cols) : rowsCount(rows), colsCount(cols)
 {
     size_t len = rowsCount * colsCount;
-    data = new T[len];
-    for (size_t i = 0; i < len; i++)
+    if (len == 0)
+        data = nullptr;
+    else
     {
-        auto value = T();
-        data[i] = value;
+        data = new T[len];
+
+        for (size_t i = 0; i < len; i++)
+        {
+            auto value = T();
+            data[i] = value;
+        }
+    }
+}
+
+template <typename T>
+inline Matrix<T>::Matrix(const Matrix &other)
+{
+    rowsCount = other.rowsCount;
+    colsCount = other.colsCount;
+    size_t len = rowsCount * colsCount;
+    if (len == 0)
+        data = nullptr;
+    else
+    {
+        data = new T[len];
+        memcpy(data, other.data, len * sizeof(T));
     }
 }
 
@@ -278,3 +363,21 @@ inline Matrix<T>::~Matrix()
 {
     delete[] data;
 }
+
+// nullptr в конструкторе и проверка.
+// 2 метода, которые защитят от Double free и write after remove memory (оператор присвоения и конструктор копирования)?
+
+// 5.3.4/7 : When the value of the expression in a noptr-new-declarator is zero, the allocation function is called to allocate an array with no elements.
+// If the value of that expression is less than zero or such that the size of the allocated object would exceed the implementation-defined limit,
+// or if the new-initializer is a braced-init-list for which the number of initializer-clauses exceeds the number of elements to initialize, no storage
+// is obtained and the new-expression terminates by throwing an exception of a type that would match a handler (15.3) of type std::bad_array_new_length (18.6.2.2).
+
+// 3.7.4.1/2
+// Even if the size of the space requested is zero, the request can fail.
+// If the request succeeds, the value returned shall be a non-null pointer value (4.10) p0 different from any previously returned value p1,
+// unless that value p1 was subsequently passed to an operator delete.
+// The effect of dereferencing a pointer returned as a request for zero size is undefined.
+
+// Сноска к 3.7.4.1/2
+// The intent is to have operator new() implementable by calling std::malloc() or std::calloc(),
+// so the rules are sub-stantially the same. C++ differs from C in requiring a zero request to return a non-null pointer.
